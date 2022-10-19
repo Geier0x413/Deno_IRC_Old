9375533fab33b59b0ed7584c24897e21c07d21ce
Geier0x413 <geier0x413@gmail.com>
2022-10-19 17:06:54 -0400

    Implemented some handy features in utility.js

--- utility.js
+++ utility.js
@@ -1,5 +1,8 @@
 import * as path from "https://deno.land/std@0.160.0/path/mod.ts";
 
+// Return an object of IRC numerical replies
+export const numerical_replies = parseJSONFile( resolvePathToProject( "/assets/numerical_replies.json" ) );
+
 // Parse JSON file and return an object
 export function parseJSONFile( file ) {
   try {
@@ -17,4 +20,19 @@ export function resolvePathToProject( file ) {
 // Force input type to be a string
 export function stringify( input ) {
   return new String( input || "" ).toString();
+}
+
+// Translate IRC replies
+export function translateIRCReply( input ) {
+  input = stringify( input ).toUpperCase();
+  input = input.match( /\d{1,3}/g ) ? input.padStart( 3 , "0" ) : input;
+  for ( let code in numerical_replies ) {
+    let reply = stringify( numerical_replies[ code ] ).toUpperCase();
+    if ( input == code ) {
+      return reply;
+    } else if ( input == reply ) {
+      return code;
+    }
+  }
+  return null;
 }
